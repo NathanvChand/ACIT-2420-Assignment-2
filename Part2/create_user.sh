8bba62689c7cd64ff50c00d2bcc34293a989c4b8
NathanvChand <156703164+NathanvChand@users.noreply.github.com>
2024-11-10 22:05:18 -0800

    Update create_user.sh
    made the bulk of the code

--- Part2/create_user.sh
+++ Part2/create_user.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+#Creating the user --------------------------------------------------------------------------------
+
+# handling errors
+error_exit() {
+    echo "Error: $1"
+    exit 1
+  }
+# user_shell is the variable we're checking if it's empty, and assigning it /bin/bash
+if [ -z "$user_shell" ]; then
+    user_shell="/bin/bash"
+fi
+
+# checks if the variable new_user is empty, will terminate the script if it is
+if [ -z "$new_user" ]; then
+    error_exit 
+fi
+
+# Creating new user with the specified shell and home directory
+useradd -m -s "$user_shell" "$new_user" || error_exit "Failed to create user $new_user."
+
+# Check if home directory was created, if not, then script is terminated
+if [ ! -d "/home/$new_user" ]; then
+    error_exit "Failed to create home directory for $new_user."
+fi
