716de05c24df6a835d174b5526c3663fac1794a4
NathanvChand <156703164+NathanvChand@users.noreply.github.com>
2024-11-10 18:22:01 -0800

    Update Linked_files.sh
    started checking for directories

--- Linked_files.sh
+++ Linked_files.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# Define source the directories
+source_bin="bin/"
+source_config="config/"
+source_bashrc="home/bashrc"
+
+# Check if source directories and files exist
+# Error handling: Check if bin/ directory exists
+if [[ ! -d "$source_bin" ]]; then
+    echo "Error: $source_bin directory not found."
+    exit 1
+fi
