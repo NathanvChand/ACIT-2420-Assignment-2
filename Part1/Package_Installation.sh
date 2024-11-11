942fcc5ad7fe5fb14ce8b1eb49c55a987a655011
NathanvChand <156703164+NathanvChand@users.noreply.github.com>
2024-11-10 17:22:19 -0800

    Update Package_Installation.sh
    made the code to download packs

--- Part1/Package_Installation.sh
+++ Part1/Package_Installation.sh
@@ -1,1 +1,16 @@
+#This file should install the user packages
+#------------------------------------------------------------------
 
+#Define your file that has the packages
+file="User Packages.txt"
+
+# Check if User Packages.txt exists
+file="User Packages.txt"
+
+# Check if User Packages.txt exists
+if [[ -f User packages.txt ]]; then
+    if sudo pacman -S --noconfirm $(cat User Packages.txt);
+else
+    echo "Error: file not found!"
+    exit 1
+fi
