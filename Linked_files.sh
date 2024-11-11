f49a3d03a41e9b9ded95f6bf2955884abbf83cc9
NathanvChand <156703164+NathanvChand@users.noreply.github.com>
2024-11-10 18:27:13 -0800

    Update Linked_files.sh
    basic error handling for the directories

--- Linked_files.sh
+++ Linked_files.sh
@@ -5,9 +5,20 @@ source_bin="bin/"
 source_config="config/"
 source_bashrc="home/bashrc"
 
-# Check if source directories and files exist
-# Error handling: Check if bin/ directory exists
+# Checking if the source directories and files exist ----------------------------------------
+# Check if bin/ directory exists
 if [[ ! -d "$source_bin" ]]; then
+    echo "Error: $source_bin directory not found."
+    exit 1
+fi
+#check if config/ directory exists-----------------------------------------------------------
+if [[ ! -d "$source_config" ]]; then
+    echo "Error: $source_bin directory not found."
+    exit 1
+fi
+
+#Check if home/bashrc exists-----------------------------------------------------------------
+if [[ ! -d "$source_bashrc" ]]; then
     echo "Error: $source_bin directory not found."
     exit 1
 fi
