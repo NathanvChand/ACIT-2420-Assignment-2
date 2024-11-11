5bcef7076b3c7af85652ebebd8630144604d32bd
NathanvChand <156703164+NathanvChand@users.noreply.github.com>
2024-11-10 20:50:10 -0800

    Update Script_caller.sh
    make sure user in root and run first script

--- Part1/Script_caller.sh
+++ Part1/Script_caller.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+#This makes sure the user has root permissions. EUID is the user id, 0 is a positional parameter.
+if [[ $EUID -ne 0 ]]; then
+  echo "USE root you menace!!!"
+  exit 1
+fi
+
+# Call the first script (downloading packages)
+echo "Starting pack download"
+./Package_Installation.sh
+
